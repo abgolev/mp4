5ab0f297cf5f50cca9171097186b4a796e36d8ec
abgolev <33265091+abgolev@users.noreply.github.com>
2018-04-03 19:41:06 -0400

    Update UpdateCenter.java
    q1

--- jenkins/core/src/main/java/hudson/model/UpdateCenter.java
+++ jenkins/core/src/main/java/hudson/model/UpdateCenter.java
@@ -555,17 +555,7 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
         Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
         for (UpdateSite site : sites) {
             for (Plugin plugin: site.getAvailables()) {
-                final Plugin existing = pluginMap.get(plugin.name);
-                if (existing == null) {
-                    pluginMap.put(plugin.name, plugin);
-                } else if (!existing.version.equals(plugin.version)) {
-                    // allow secondary update centers to publish different versions
-                    // TODO refactor to consolidate multiple versions of the same plugin within the one row
-                    final String altKey = plugin.name + ":" + plugin.version;
-                    if (!pluginMap.containsKey(altKey)) {
-                        pluginMap.put(altKey, plugin);
-                    }
-                }
+                tryAddPluginToMap(pluginMap, plugin);
             }
         }
 
@@ -603,22 +593,26 @@ public class UpdateCenter extends AbstractModelObject implements Saveable, OnMas
         Map<String,Plugin> pluginMap = new LinkedHashMap<String, Plugin>();
         for (UpdateSite site : sites) {
             for (Plugin plugin: site.getUpdates()) {
-                final Plugin existing = pluginMap.get(plugin.name);
-                if (existing == null) {
-                    pluginMap.put(plugin.name, plugin);
-                } else if (!existing.version.equals(plugin.version)) {
-                    // allow secondary update centers to publish different versions
-                    // TODO refactor to consolidate multiple versions of the same plugin within the one row
-                    final String altKey = plugin.name + ":" + plugin.version;
-                    if (!pluginMap.containsKey(altKey)) {
-                        pluginMap.put(altKey, plugin);
-                    }
-                }
+                tryAddPluginToMap(pluginMap, plugin);
             }
         }
 
         return new ArrayList<Plugin>(pluginMap.values());
     }
+
+	public void tryAddPluginToMap(Map<String, Plugin> pluginMap, Plugin plugin) {
+		final Plugin existing = pluginMap.get(plugin.name);
+		if (existing == null) {
+		    pluginMap.put(plugin.name, plugin);
+		} else if (!existing.version.equals(plugin.version)) {
+		    // allow secondary update centers to publish different versions
+		    // TODO refactor to consolidate multiple versions of the same plugin within the one row
+		    final String altKey = plugin.name + ":" + plugin.version;
+		    if (!pluginMap.containsKey(altKey)) {
+		        pluginMap.put(altKey, plugin);
+		    }
+		}
+	}
     
     /**
      * Ensure that all UpdateSites are up to date, without requiring a user to
